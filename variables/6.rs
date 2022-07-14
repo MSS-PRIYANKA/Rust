9e42caa0cc4d71524bad62ba6fbf4a8c6ac14143
MSS-PRIYANKA <95180619+MSS-PRIYANKA@users.noreply.github.com>
2022-07-14 18:01:17 +0530

    6

---
+++ variables/6.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x: i32 = 12;
+    {
+        let x = 5;
+        assert_eq!(x, 5);
+    }
+
+    assert_eq!(x, 12);
+
+    let x =  42;
+    println!("{}", x); // Prints "42".
+}
