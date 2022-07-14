7060becf2182e9c7138cc6cb13327ac7d7bc7caa
MSS-PRIYANKA <95180619+MSS-PRIYANKA@users.noreply.github.com>
2022-07-14 15:06:48 +0530

    4

---
+++ struct/4.rs
@@ -0,0 +1,24 @@
+
+// Fix the error and fill the blanks
+
+// Fill the blank and fix the error without adding/removing new line
+struct Person {
+    name: String,
+    age: u8,
+}
+fn main() {
+    let age = 18;
+    let mut p = Person {
+        name: String::from("sunface"),
+        age,
+    };
+
+    // How can you believe sunface is only 18? 
+    p.age = 30;
+
+    // Fill the blank
+    p.name = String::from("sunfei");
+
+    println!("Success!");
+}
+
